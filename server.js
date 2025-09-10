e107f7746ee231cd48e1b8f183a4c52dbca0a648
leonilo ondangan <ondanganleonilo61@gmail.com>
2025-09-10 18:38:53 +0800

    adding comment berdisola in server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const DATA_FILE = "./athletes.json";
 const SECRET_KEY = "mysecretkey"; // change to env variable in production
 
-// Load data from file or initialize empty by jason 
+// Load data from file or initialize empty by Berdisola 
 let athletes = [];
 if (fs.existsSync(DATA_FILE)) {
   athletes = JSON.parse(fs.readFileSync(DATA_FILE));
